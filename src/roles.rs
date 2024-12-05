10399bdc834f29edfc035740c3e60c60a456676e
Wabtey <wabtey@disroot.org>
2024-12-05 12:50:16 +0100

    refactor: :truck: :sparkles: Role Selection and modularize features

--- src/roles.rs
+++ src/roles.rs
@@ -1,20 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Default, Debug)]
-pub struct ConstructionSite {
-    name: String,
-    coordinates: (f32, f32),
-    start_day: usize,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum Role {
-    /// # Notes
-    ///
-    /// fr = chef de chantier
-    SiteManager,
-    /// # Notes
-    ///
-    /// fr = responsable des chantiers
-    SitesGlobalManager,
+pub struct Worker {
+    pub name: String,
 }
